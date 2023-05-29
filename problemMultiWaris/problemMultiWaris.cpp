90527b791dc65beae767555133ce4710da8ef68e
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-29 10:45:53 +0700

    Membuat turunan dari class orang yaitu class pelajar beserta dengan methodnya

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -19,6 +19,14 @@ public:
     }
 };
 
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
