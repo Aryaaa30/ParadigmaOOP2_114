4a3bf8d159988afa1199adb6c18db66085dcab41
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-29 12:53:10 +0700

    Membuat class berupa baseClass beserta dengan methodnya

---
+++ programOverriding/programOverriding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Halo saya function dari base class";
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
